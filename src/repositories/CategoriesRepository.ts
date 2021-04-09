a117bc3836238cb6d73ef23e3348bf10dacaa9de
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2021-04-08 21:54:12 -0300

    Validando o cadastro de categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -25,5 +25,10 @@ class CategoriesRepository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
+    return category;
+  }
 }
 export { CategoriesRepository };
