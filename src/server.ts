a3b56f457b0a8aee309faf934a27a2e2903b7e2e
Fernando <fernandoselvino@gmail.com>
2021-04-18 17:59:58 -0300

    Criando documentaçao de criaçao de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { routers } from './routes';
 
+// habilitar resolveJsonModule no arquivo de configuração tsconfig
+import swaggerFile from './swagger.json';
+
 const app = express();
+
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(routers);
 
 app.listen(3333, () => {
