b6e55beca7f66cc7b12efe36fcc434efa2338bcc
Fernando <fernandoselvino@gmail.com>
2021-04-25 12:45:47 -0300

    Criando repositorio de usuarios

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,11 @@
+export interface ICreateUserDTO {
+  name: string;
+
+  username: string;
+
+  password: string;
+
+  email: string;
+
+  driver_license: string;
+}
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implemetations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
@@ -18,4 +20,9 @@ container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   // Categoria a ser injetada
   SpecificationsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
