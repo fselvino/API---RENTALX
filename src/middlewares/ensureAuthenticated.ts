1f4208d0203d70921daedff64b7d46c4ba9f139d
Fernando <fernandoselvino@gmail.com>
2021-04-25 21:52:26 -0300

    Autenticaçao nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '../modules/accounts/repositories/implemetations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('Token missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      '452a9b21da3b02504dc067a6f121c4c9',
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error('Invalid token!');
+    }
+
+    next();
+  } catch (error) {
+    throw new Error('Invalid token!');
+  }
+}
